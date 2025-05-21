8c1c2cc0b15727a58244009b52f9bccb2a37b479
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-05-21 15:04:03 -0500

    Modificando el home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import HeaderComponent from '../components/HeaderComponent';
 function HomePage(){
     return (
         <>
-            <HeaderComponent />
+            
             <div className="container mt-3">
                 <div className="border-bottom pb-3 mb-3">
                     <h3>Home</h3>
